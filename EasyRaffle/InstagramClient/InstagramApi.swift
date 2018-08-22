c9d8a909887011d758a1eefb033ca7ec05a5c163
AlexanderOsokin <tureckiy@gmail.com>
2018-08-22 16:12:31 +0300

    insta lib init

---
+++ EasyRaffle/InstagramClient/InstagramApi.swift
@@ -0,0 +1,9 @@
+//
+//  InstagramApi.swift
+//  InstagramClient
+//
+//  Created by Edu user on 22/08/2018.
+//  Copyright © 2018 Alexander Osokin. All rights reserved.
+//
+
+import Foundation
---
+++ EasyRaffle/InstagramClient/InstagramClient/InstagramClient.h
@@ -0,0 +1,19 @@
+//
+//  InstagramClient.h
+//  InstagramClient
+//
+//  Created by Edu user on 22/08/2018.
+//  Copyright © 2018 Alexander Osokin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InstagramClient.
+FOUNDATION_EXPORT double InstagramClientVersionNumber;
+
+//! Project version string for InstagramClient.
+FOUNDATION_EXPORT const unsigned char InstagramClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InstagramClient/PublicHeader.h>
+
+
